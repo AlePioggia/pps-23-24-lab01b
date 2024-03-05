e730f7edfc4bb285529672c3b0cb42db2ed9a824
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-03-05 12:22:37 +0100

    addBasicTests

--- src/main/java/e1/Logics.java
+++ src/main/java/e1/Logics.java
@@ -1,27 +1,40 @@
 package e1;
 
-public interface Logics{
-    
+public interface Logics {
+
     /**
      * attempt to move Knight on position row,col, if possible
      * 
      * @param row
      * @param col
-     * @return whether the pawn has been hit 
+     * @return whether the pawn has been hit
      */
     boolean hit(int row, int col);
-    
+
     /**
      * @param row
      * @param col
      * @return whether position row,col has the knight
      */
     boolean hasKnight(int row, int col);
-    
+
     /**
      * @param row
      * @param col
      * @return whether position row,col has the pawn
      */
     boolean hasPawn(int row, int col);
+
+    /**
+     * 
+     * @return pawn's position
+     */
+    Pair<Integer, Integer> getPawn();
+
+    /**
+     * 
+     * @return Knight's position
+     */
+    Pair<Integer, Integer> getKnight();
+
 }
--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,15 +1,51 @@
 package e1;
+
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map.Entry;
+import java.util.function.Predicate;
+
+import javax.swing.JButton;
+
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
+  private static final int GRID_SIZE = 5;
+  private static final int KNIGHT_X_COORDINATE = 1;
+  private static final int KNIGHT_Y_COORDINATE = 4;
+  private static final int PAWN_X_COORDINATE = 0;
+  private static final int PAWN_Y_COORDINATE = 2;
+
+  private Logics logics;
+
+  @BeforeEach
+  public void setUp() {
+    this.logics = new LogicsImpl(GRID_SIZE, new Pair<Integer, Integer>(KNIGHT_X_COORDINATE, KNIGHT_Y_COORDINATE),
+        new Pair<Integer, Integer>(PAWN_X_COORDINATE, PAWN_Y_COORDINATE));
   }
+
+  @Test
+  public void testHasPawn() {
+    boolean checkRightPosition = this.logics.hasPawn(PAWN_X_COORDINATE, PAWN_Y_COORDINATE);
+    boolean checkWrongPosition = this.logics.hasPawn(0, 0);
+    assertTrue(checkRightPosition);
+    assertFalse(checkWrongPosition);
+  }
+
+  @Test
+  public void testHasKnight() {
+    boolean checkRightPosition = this.logics.hasKnight(GRID_SIZE, KNIGHT_X_COORDINATE);
+    boolean checkWrongPosition = this.logics.hasKnight(0, 0);
+    assertTrue(checkRightPosition);
+    assertFalse(checkWrongPosition);
+  }
+
+  @Test
+  public void testPiecesOverlap() {
+    Pair<Integer, Integer> pawn = this.logics.getPawn();
+    Pair<Integer, Integer> knight = this.logics.getKnight();
+    boolean arePositionsDifferent = pawn.getX() != knight.getX() || pawn.getY() != knight.getY();
+    assertTrue(arePositionsDifferent);
+  }
+
 }
